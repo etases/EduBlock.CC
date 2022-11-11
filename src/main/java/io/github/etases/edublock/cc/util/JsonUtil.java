50e4623643e9025a4d435599b5ce6ac2cf9d253d
HSGamer <huynhqtienvtag@gmail.com>
2022-11-11 15:06:51 +0700

    should fail on missing property

--- src/main/java/io/github/etases/edublock/cc/util/JsonUtil.java
+++ src/main/java/io/github/etases/edublock/cc/util/JsonUtil.java
@@ -1,11 +1,16 @@
 package io.github.etases.edublock.cc.util;
 
 import com.owlike.genson.Genson;
+import com.owlike.genson.GensonBuilder;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class JsonUtil {
-    private static final Genson genson = new Genson();
+    private static final Genson genson = new GensonBuilder()
+            .useRuntimeType(true)
+            .failOnMissingProperty(true)
+            .failOnNullPrimitive(true)
+            .create();
 
     public static <T> T deserialize(String jsonString, Class<T> clazz) {
         return genson.deserialize(jsonString, clazz);
--- src/test/java/io/github/etases/edublock/cc/util/JsonUtilTest.java
+++ src/test/java/io/github/etases/edublock/cc/util/JsonUtilTest.java
@@ -1,6 +1,8 @@
 package io.github.etases.edublock.cc.util;
 
+import com.owlike.genson.JsonBindingException;
 import io.github.etases.edublock.cc.model.Classification;
+import io.github.etases.edublock.cc.model.Subject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,11 +11,27 @@ class JsonUtilTest {
 
     @Test
     void deserialize() {
+        String jsonString = "{\"name\":\"TestSubject\",\"firstHalfScore\":9.0,\"secondHalfScore\":9.5,\"finalScore\":10.0}";
+        Subject subject = JsonUtil.deserialize(jsonString, Subject.class);
+        assertEquals("TestSubject", subject.getName());
+        assertEquals(9.0, subject.getFirstHalfScore());
+        assertEquals(9.5, subject.getSecondHalfScore());
+        assertEquals(10.0, subject.getFinalScore());
+    }
+
+    @Test
+    void deserializeNullProperty() {
         String jsonString = "{\"firstHalfClassify\":\"Good\",\"secondHalfClassify\":\"Bad\"}";
         Classification classification = JsonUtil.deserialize(jsonString, Classification.class);
         assertEquals("Good", classification.getFirstHalfClassify());
         assertEquals("Bad", classification.getSecondHalfClassify());
         assertNull(classification.getFinalClassify());
+    }
+
+    @Test
+    void deserializeException() {
+        String jsonString = "{\"name\":\"TestSubject\",\"firstHalfScore\":null,\"secondHalfScore\":\"test\",\"finalScore\":10.0}";
+        assertThrows(JsonBindingException.class, () -> JsonUtil.deserialize(jsonString, Subject.class));
     }
 
     @Test
